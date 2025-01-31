dc708647021859eff74ae2b183c6772ca1130334
NGBAMA William <william.ngbama@gmail.com>
2025-01-31 07:11:04 +0100

    remove .history

--- .history/vite.config_20250131064750.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { unisonVue } from 'unplugin-unisonjs/vite'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [unisonVue(), react()],
-})
